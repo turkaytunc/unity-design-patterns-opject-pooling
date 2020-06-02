47a532d13f3384fdcaab9c3da2c6f427e18c0e1f
Turkay Tunc <turkaytunc00@gmail.com>
2020-06-02 22:15:03 +0300

    added object pooling simple example

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject _objectToSpawn;//used to spawn the Bullets if _poolType is set to None
+    public Transform _bulletSpawnPos;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            SpawnBullet();
+    }
+
+    private void SpawnBullet()
+    {
+
+        GameObject newBullet = Instantiate(_objectToSpawn, _bulletSpawnPos.position, _bulletSpawnPos.rotation);
+    }
+
+}
---
+++ Assets/Scripts/Not Important/ZombieSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+
+    public GameObject _zombiePrefab;
+
+    public float _spawnInterval = 0.5f;
+    private float _spawnTimer;
+
+	
+	// Update is called once per frame
+	void Update () {
+        _spawnTimer += Time.deltaTime;
+        if(_spawnTimer > _spawnInterval)
+        {
+            _spawnTimer = 0;
+            SpawnZombie();
+        }
+	}
+
+    void SpawnZombie()
+    {
+        Vector3 rndSpawnPos = new Vector3(Random.Range(-8f, 8f), 0, 30);
+
+        Instantiate(_zombiePrefab, rndSpawnPos, Quaternion.Euler(0, 180, 0));
+    }
+}
